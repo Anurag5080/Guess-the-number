38279a3a68be0eb31677dca817ad0da57092d0a4
Anurag Singh <141731191+Anurag5080@users.noreply.github.com>
2025-01-16 18:30:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let randomNo = parseInt((Math.random()) * 100 + 1);
 const userinput = document.getElementById('userinput');
 const submitbtn = document.getElementById('submitbtn');
 const restartbtn = document.getElementById('restartbtn');
-
 const remainattempt = document.getElementById('remain-attempt');
 
 const guessarrayjs = document.getElementById('guessarray');
